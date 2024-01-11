7358f9d035220a860867225bb088868cfecc3322
Thomas Leisser <thomas.leisser@htlstp.at>
2024-01-11 17:25:57 +0100

    Initial commit

---
+++ src/main/java/net/thomas24m/lifesteal/LifeSteal.java
@@ -0,0 +1,16 @@
+package net.thomas24m.lifesteal;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LifeSteal implements ModInitializer {
+	public static final String MOD_ID = "lifesteal";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/thomas24m/lifesteal/LifeStealClient.java
@@ -0,0 +1,10 @@
+package net.thomas24m.lifesteal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LifeStealClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
